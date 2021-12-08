8f8a260bdd7ad59f3f48293b3cab3e78a13b73f6
shubhamchugh <chugh.shubham12@gmail.com>
2021-12-08 16:24:26 +0530

    Bug Fix

--- resources/views/themes/BootstrapSimple/content/post.blade.php
+++ resources/views/themes/BootstrapSimple/content/post.blade.php
@@ -92,11 +92,11 @@
             <div class="row my-3">
                 <h3>FAQ</h3>
                 <div class="col-md-6">
-                    {{ faq(3,$content->content_title) }}
+                    {{ faq(3, $post->post_title) }}
                 </div>
 
                 <div class="col-md-6">
-                    {{ faq(3,$content->content_title) }}
+                    {{ faq(3, $post->post_title) }}
                 </div>
             </div>
 
