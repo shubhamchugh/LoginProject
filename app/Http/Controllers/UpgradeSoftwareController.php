fc1251cace665667199056582f68c01872445426
Shubham chugh <chugh.shubham12@gmail.com>
2022-05-04 18:19:07 +0530

    git security patch update in Upgrade System

--- app/Http/Controllers/UpgradeSoftwareController.php
+++ app/Http/Controllers/UpgradeSoftwareController.php
@@ -16,6 +16,7 @@ class UpgradeSoftwareController extends Controller
     public function __invoke(Request $request)
     {
         echo "<pre>";
+        echo shell_exec("git config --global --add safe.directory '*'");
         echo shell_exec('cd .. && git status');
 
         echo shell_exec('cd .. && sudo chmod -R o+rw bootstrap/cache');
@@ -25,7 +26,7 @@ class UpgradeSoftwareController extends Controller
         echo shell_exec('cd .. && sudo chmod -R 777 public');
         echo shell_exec('cd .. && sudo chmod -R o+rw public');
 
-        echo shell_exec('cd .. && git reset --hard && git pull');
+        echo shell_exec('cd .. && git reset --hard && git clean -d -f && git pull');
 
         echo shell_exec('cd .. && COMPOSER_MEMORY_LIMIT=-1 composer update');
         echo shell_exec('cd .. && php artisan migrate');
