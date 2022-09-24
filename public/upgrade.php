ec5d3e45d36544016aac2100b051d538abd83a30
ShubhamChugh OS 22.04 <chugh.shubham12@gmail.com>
2022-09-24 13:36:20 +0000

    allow redirection

--- public/upgrade.php
+++ public/upgrade.php
@@ -82,6 +82,7 @@ $curl = curl_init();
  curl_setopt($curl, CURLOPT_URL, "$currentDomain_sql"); // set live website where data from
  curl_setopt($curl, CURLOPT_RETURNTRANSFER, TRUE); // default
  curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE); // default
+ curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
  curl_setopt($curl, CURLOPT_TIMEOUT, 60); //timeout in seconds
  $content = curl_exec($curl);
 
